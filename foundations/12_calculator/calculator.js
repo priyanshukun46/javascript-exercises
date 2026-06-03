6d7ef425ebebfcd84a5b1e92ff35fcb79156902c
priyanshukun46 <pkfb46@gmail.com>
2026-06-04 01:30:07 +0530

    Update calculator.js

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
+const add = function (a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sum = 0;
+  let sumarr = arr.map(s => {
+    sum += s;
+  })
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) return 1;
+  return a * (factorial(a - 1));
 };
 
 // Do not edit below this line
