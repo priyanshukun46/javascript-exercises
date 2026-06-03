6b89f4b5841700b7aa8a0a7e5faad04917117e25
priyanshukun46 <pkfb46@gmail.com>
2026-06-04 01:05:29 +0530

    09 done

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  if (a < 0 || b < 0) return "ERROR";
+  let sum = 0;
+  if (a < b) {
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+  }
+  else {
+    let temp = a;
+    a = b;
+    b = temp;
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+    
+  }
+  return sum;
 };
 
 // Do not edit below this line
